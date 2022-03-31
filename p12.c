7d1cb1ad577d33092959ed60c3f4d4be8be0d287
Pavankalyan08 <96220569+Pavankalyan08@users.noreply.github.com>
2022-03-31 16:11:09 +0000

    p12.c

--- p12.c
+++ p12.c
@@ -1,1 +1,34 @@
-
+#include<stdio.h>
+#include<math.h>
+int input()
+{
+  int n;
+  printf("enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  int fibo=0;
+  int a=0;
+  int b=1;
+  for(int i=0;i<n;i++)
+    {
+      fibo=a;
+      a=b;
+      b=fibo+b;
+    }
+  return fibo;
+}
+void output(int n,int fibo)
+{
+  printf("fibonacci sequence of %d is %d \n",n,fibo);
+}
+int main()
+{
+  int n,fibo;
+  n=input(n);
+  fibo=find_fibo(n);
+  output(n,fibo);
+  return 0;
+}
